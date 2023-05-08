dbc3150cdf5921d869c3b0adb02da9f24b90962c
Prasad Kadam <prasadkadam272002@fgmail.com>
2023-05-08 17:03:34 +0530

    initial commit

---
+++ Controllers/CategoryController.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Crud.Controllers
+{
+    public class CategoryController : Controller
+    {
+        Product_Management_System_DBEntities Context = new Product_Management_System_DBEntities();
+
+        // GET: Category
+        public ActionResult Index()
+        {
+            var listofData = Context.Categories.ToList();
+            return View(listofData);
+        }
+
+        // GET: Category/Details/5
+        public ActionResult Details(int id)
+        {
+            var data = Context.Categories.Where(x => x.CategoryID == id).FirstOrDefault();
+            return View(data);
+        }
+
+        // GET: Category/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Category/Create
+        [HttpPost]
+        public ActionResult Create(Category model)
+        {
+            try
+            {
+                Context.Categories.Add(model);
+                Context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Category/Edit/5
+        public ActionResult Edit(int id)
+        {
+            var data = Context.Categories.Where(x => x.CategoryID == id).FirstOrDefault();
+            return View(data);
+        }
+
+        // POST: Category/Edit/5
+        [HttpPost]
+        public ActionResult Edit( Category model)
+        {
+            var data = Context.Categories.Where(x => x.CategoryID == model.CategoryID).FirstOrDefault();
+
+            if (data != null)
+            {
+                data.CategoryName = model.CategoryName;
+                Context.SaveChanges();
+            }
+            return RedirectToAction("Index");
+        }
+
+        // GET: Category/Delete/5
+        public ActionResult Delete(int id)
+        {
+            var data = Context.Categories.Where(x => x.CategoryID == id).FirstOrDefault();
+            Context.Categories.Remove(data);
+            Context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ Controllers/HomeController.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Crud.Controllers
+{
+    public class HomeController : Controller
+    {
+        Product_Management_System_DBEntities Context = new Product_Management_System_DBEntities();
+
+        public ActionResult Index()
+        {
+            var listofData = Context.ProductTables.ToList();
+            return View(listofData);
+        }
+
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(ProductTable model)
+        {
+            Context.ProductTables.Add(model);
+            Context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public ActionResult Edit(int Id)
+        {
+            var data = Context.ProductTables.Where(x => x.ProductID == Id).FirstOrDefault();
+            return View(data);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(ProductTable model)
+        {
+            var data = Context.ProductTables.Where(x => x.ProductID == model.ProductID).FirstOrDefault();
+
+            if(data != null)
+            {
+                data.ProductName = model.ProductName;
+                data.CategoryID = model.CategoryID;
+                Context.SaveChanges();
+            }
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Details(int id)
+        {
+            var data = Context.ProductTables.Where(x => x.ProductID == id).FirstOrDefault();
+            return View(data);
+        }
+
+        public ActionResult Delete(int id)
+        {
+            var data = Context.ProductTables.Where(x => x.ProductID == id).FirstOrDefault();
+            Context.ProductTables.Remove(data);
+            Context.SaveChanges();
+            return RedirectToAction("Index");
+
+        }
+    }
+}
